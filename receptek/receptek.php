fb4adfaa9f3a845276222dc1ecadd88459c06060
mszrsakos <meszarosakos@turr.hu>
2026-01-22 13:24:53 +0100

    footer javitasa+ index h1 javitasa,szepitese

--- receptek/receptek.php
+++ receptek/receptek.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="receptek.css">
-    <link rel="stylesheet" href="/munchies/header/header.css">
+    <link rel="stylesheet" href="../header/header.css">
+    <link rel="stylesheet" href="../footer/footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
@@ -65,49 +66,38 @@
     <!-- tartalom vege -->
 
 
-    
+    <!--footer  -->
 
-
-
-
-    <footer>
-        <div id="footerFelso">
-            <div id="logo"><img src="../imgs/munchieslogo.png" id="logoImg"></div>
-            <div id="socialmedia">
-                <div id="imgsRow">
-                    <img src="../imgs/facebooklogo.png" alt="">
-                    <img src="../imgs/instagramlogo.png" alt="">
-                    <img src="../imgs/youtubelogo.png" alt="">
-                </div>
-            </div>
-            <div id="jobboldal">
-                <div id="kapcsolat">
-                    <p>Adatvédelem</p>
-                    <p>Kapcsolatfelvétel</p>
-                    <p>Felhasználói feltételek</p>
-                    <p>Kapcsolat</p>
-                </div>
-                <!-- <div id="hirlevel">
-                    <input type="text" placeholder="E-mail címed">
-                    <button type="submit" style="height: min-content; width: 70%;">Iratkozz fel hírlevelünkre!</button>
-                </div> -->
-                
-                    <form class="hirlevel">
-                        <input 
-                          type="email" 
-                          placeholder="Írd be az e-mail címed" 
-                          aria-label="Email address"
-                          required
-                        >
-                        <button type="submit">Feliratkozás</button>
-                      </form>
-                
+<footer>
+    <div id="footerFelso">
+        <div id="logo"><img src="../imgs/munchieslogo.png" id="logoImg"></div>
+        <div id="socialmedia">
+            <div id="imgsRow">
+                <a href=""><img src="../imgs/facebooklogo.png" alt=""></a>
+                <a href=""><img src="../imgs/instagramlogo.png" alt=""></a>
+                <a href=""><img src="../imgs/youtubelogo.png" alt=""></a>
             </div>
         </div>
-        <div id="footerAlso">
-            <p>2025 Munchies · Bajai SZC Türr István Technikum<br>Minden jog fenntartva</p>
+        <div id="jobboldal">
+            <div id="kapcsolat">
+                <p>Adatvédelem</p>
+                <p>Kapcsolatfelvétel</p>
+                <p>Felhasználói feltételek</p>
+                <p>Kapcsolat</p>
+            </div>
+            <div id="hirlevel">
+                <input type="text" placeholder="E-mail címed">
+                <button style="height: min-content; width: 70%;">Iratkozz fel hírlevelünkre!</button>
+            </div>
         </div>
-    </footer>
+    </div>
+    <div id="footerAlso">
+        <p>2025 Munchies · Bajai SZC Türr István Technikum<br>Minden jog fenntartva</p>
+    </div>
+</footer>
+
+
+    <!-- footer vege -->
     <script src="main.js"></script>
 </body>
 </html>
