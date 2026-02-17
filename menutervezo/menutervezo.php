4062f299260fc6f89b1465f6bf7de7693ace565c
melcherlevente <melcherlevente@turr.hu>
2026-02-17 12:30:15 +0100

    SCRUM-25 menutervezo tabla

--- menutervezo/menutervezo.php
+++ menutervezo/menutervezo.php
@@ -11,8 +11,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menütervező</title>
-    <link rel="stylesheet" href="menutervezo.css">
     <link rel="stylesheet" href="../header/header.css">
+    <link rel="stylesheet" href="menutervezo.css">
     <link rel="stylesheet" href="../footer/footer.css">
     <link rel="icon" type="image/x-icon" href="../imgs/munchieslogo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -23,51 +23,27 @@
 <body>
 
     <?php include("../header/header.html");?>
+    <main>
+        <div class="grid-table">
+            <!-- Headers -->
+            <div></div>
+            <div class="grid-header">Reggeli</div>
+            <div class="grid-header">Ebéd</div>
+            <div class="grid-header">Vacsora</div>
+            <div class="grid-header">Egyéb</div>
 
-    <div class="grid-container">
-        <div class="row">
-            <div class="cell">Hétfő</div>
-            <div class="cell">Kedd</div>
-            <div class="cell">Szerda</div>
-            <div class="cell">Csütörtök</div>
-            <div class="cell">Péntek</div>
+            <!-- Row 1 -->
+            <div class="grid-header">R1 C1</div><div>R1 C2</div><div>R1 C3</div><div>R1 C4</div><div>R1 C5</div>
+            <!-- Row 2 -->
+            <div class="grid-header">R2 C1</div><div>R2 C2</div><div>R2 C3</div><div>R2 C4</div><div>R2 C5</div>
+            <!-- Row 3 -->
+            <div class="grid-header">R3 C1</div><div>R3 C2</div><div>R3 C3</div><div>R3 C4</div><div>R3 C5</div>
+            <!-- Row 4 -->
+            <div class="grid-header">R4 C1</div><div>R4 C2</div><div>R4 C3</div><div>R4 C4</div><div>R4 C5</div>
+            <!-- Row 5 -->
+            <div class="grid-header">R5 C1</div><div>R5 C2</div><div>R5 C3</div><div>R5 C4</div><div>R5 C5</div>
         </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-        <div class="row">
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-            <div class="cell"></div>
-        </div>
-    </div>
+    </main>
 
 
     <?php include("../footer/footer.html"); ?>
